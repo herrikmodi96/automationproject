9c09e610f11baf0290215ba538b68708a933c56a
herrikmodi96 <herrikmodi96@gmail.com>
2024-04-09 18:51:19 -0600

    Added Proper Comments in code and added Readme file

---
+++ README.md
@@ -0,0 +1,76 @@
+# Introduction
+This repository contains automation tests developed to fulfill a tech assignment's requirements. The project utilizes Maven, TestNG, Selenium WebDriver, and Java. The tests are executed in Visual Studio Code.
+
+# Project Structure
+- src/main/java: Contains the main Java source files.
+- src/test/java: Contains the test Java source files.
+- src/Resources: Contains the test HTML files and other resources.
+- pom.xml: Maven configuration file.
+
+# Dependencies
+Ensure you have the following dependencies installed:
+
+- Java Development Kit (JDK)
+- Maven
+- TestNG
+- Selenium WebDriver
+- ChromeDriver
+
+# Setup
+1. Clone the repository to your local machine.
+2. Set up the project in your preferred Java IDE (e.g., Visual Studio Code, IntelliJ IDEA).
+3. Configure the project with Maven and ensure all dependencies are resolved.
+4. Download and configure the ChromeDriver according to your operating system.
+
+# Implemented Methods
+- @BeforeClass Method: This method initializes the WebDriver and sets up the test environment before each test execution. It maximizes the browser window and waits for 2 seconds.
+
+- @AfterClass Method: This method quits the WebDriver instance and closes the browser after all tests are executed.
+
+## Implemented Tests
+
+## Test1 
+
+- Navigate to the Home Page: Opens the web page in the browser, through @BeforeClass
+- Assert Element Presence: Finds email input field, password input field and Login button through properly formatted selectors, and assert their presence through assertTrue Method of TetNG
+- Enter Value in Email & Password field: Fills Email and password field through sendkeys method
+
+## Test2- testListGroupValidation
+
+- Navigate to the Home Page: Ensures the browser is on the home page
+- Assert list items: After finding Group list element and the list items in it, This test will check if the list has 3 items,if the listgroup has less or more than 3 items in it, test will fail and show a failure message on console 
+- Assert Value and Badge: Verifies second list item's value is 'List item 2' and second list item's badge value is 6
+
+## Test3- testSelectOptionFunctionality
+
+- Navigate to the Home Page: Ensures the browser is on the home page
+- Assert default selection: After finding dropdown element, Test3 checks default value is Option 1, through assertEquals
+- Select Option 3- After finding Option 3 element, it selects Options 3 and asserts that the selected value is Option 3
+
+## Test4- testButtonStateValidation
+
+- Navigate to the Home Page: Ensures the browser is on the home page
+- Assert button states: After finding both button elements, Test4 checks if the first button is enabled and the second button is disabled
+
+## Test5- testButtonInteraction
+
+- Navigate to the Home Page: Ensures the browser is on the home page
+- Wait for button display: Using WebdriverWait method, test assigns wait time of 20 seconds to wait variable. Using this wait variable, visibilityOfElementLocated method is used to wait for the button to appear until 20 seconds
+- Click Button: clicks the button after it becomes visible
+- Assert Success message: Checks if success message is displayed
+- Assert Button State: confirms if the button is disabled
+
+## Test6- testGridCellValueRetrieval
+- Navigate to the Home Page: Ensures the browser is on the home page
+- Value retrival method: getCellValue method, which is created to find value of any cell of grid format
+- Value retrival: Utilizing getCellValue, retrives the value of the cell at coordinates (2,2)
+- Assert Cell value: Asserts the vlaue of the cell at (2,2) is "Ventosanzap"
+
+# Execution
+Execute the test suite using TestNG. Make sure the ChromeDriver is properly configured and available in the system PATH.
+
+# Testing Environment
+
+Operating System: Mac OS
+Browser: Google Chrome [123.0.6312.107]
+Java Version: [22]
--- src/test/java/com/qa/hmautomation/Assignment.java
+++ src/test/java/com/qa/hmautomation/Assignment.java
@@ -18,70 +18,87 @@ public class Assignment {
 
     @BeforeClass
     public void setUp() {
+        //Setting chromedriver path and initialize WebDriver
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/Resources/chromedriver");
         driver = new ChromeDriver();
+
+        //Maximize browser window
         driver.manage().window().maximize();
+        //Navigate to test on Home page 
         driver.get("file://" + System.getProperty("user.dir") + "/src/Resources/QE-index.html");
+        try {
+            Thread.sleep(2000); // Wait for 2 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(priority = 1)
     public void testUserAuthentication() {
         
-
+        // Find and initialize email, password and login button elements
         WebElement emailInput = driver.findElement(By.id("inputEmail"));
         WebElement passwordInput = driver.findElement(By.id("inputPassword"));
         WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(),'Sign in')]"));
 
+        //Asserting email, password and login button, all elements are present
         Assert.assertTrue(emailInput.isDisplayed() && passwordInput.isDisplayed() && loginButton.isDisplayed(),
                 "Email and password inputs along with login button are not present.");
 
-        emailInput.sendKeys("test@example.com");
-        passwordInput.sendKeys("password");
+        // Enter Email and password, then click login button
+        emailInput.sendKeys("hmtest1@example.com");
+        passwordInput.sendKeys("hmtest1-password");
         loginButton.click();
 
-        // Pass message
-        System.out.println("Test 1 - User Authentication: Passed");
+       
     }
 
     @Test(priority = 2)
     public void testListGroupValidation() {
-        
+        //Find and initialize list group and list items
         WebElement listGroup = driver.findElement(By.xpath("//div[@id='test-2-div']/ul[1]"));
         java.util.List<WebElement> listItems = listGroup.findElements(By.tagName("li"));
 
+        //Assert numbers in list items
         Assert.assertEquals(listItems.size(), 3, "Incorrect number of list items.");
         
-
+        // Find second list item and its badge
         WebElement secondListItem = driver.findElement(By.xpath("//div[@id='test-2-div']/ul[1]/li[2]"));
         String actualtext=secondListItem.getText();
         //System.out.println(actualtext);
         String expectedtText="List Item 2";
-    
+        
+        //Assert the value of the scond list item
         Assert.assertTrue(actualtext.contains(expectedtText), "Second list item's value is incorrect.");
 
+        //Using secondListItem webelement, find badge of secondListItem
         WebElement badge = secondListItem.findElement(By.xpath("//span[contains(text(),'6')]"));
+        //Assert the badge value of the scond list item
         Assert.assertEquals(badge.getText(), "6", "Second list item's badge value is incorrect.");
     }
 
     @Test(priority = 3)
     public void testSelectOptionFunctionality() {
-        
+        //Find dropdown button
         WebElement dropdownButton = driver.findElement(By.xpath("//button[@id='dropdownMenuButton']"));
+        //Assert the default selected value
         Assert.assertEquals(dropdownButton.getText(), "Option 1", "Default selected value is not 'Option 1'.");
         
+        //Click dropdown button and select option 3
         dropdownButton.click();
         WebElement option3 = driver.findElement(By.xpath("//div[@class='dropdown-menu show']//a[contains(text(),'Option 3')]"));
         option3.click();
-        
+        //Assert the selected value
         Assert.assertEquals(dropdownButton.getText(), "Option 3", "Selected value is not 'Option 3'.");
     }
 
     @Test(priority = 4)
     public void testButtonStateValidation() {
-        
+        //Find both buttons
         WebElement firstButton = driver.findElement(By.xpath("//div[@id='test-4-div']//button[@class='btn btn-lg btn-primary']"));
         WebElement secondButton = driver.findElement(By.xpath("//div[@id='test-4-div']//button[@class='btn btn-lg btn-secondary']"));
 
+        //Assert button states
         Assert.assertTrue(firstButton.isEnabled(), "The first button is not enabled.");
         Assert.assertFalse(secondButton.isEnabled(), "The second button is not disabled.");
     }
@@ -91,24 +108,28 @@ public class Assignment {
         
 
         // Wait for the button to be displayed (with random delay)
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement test5button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='test5-button']")));
 
-        //WebElement test5button = driver.findElement(By.xpath("//button[@id='test5-button']"));
+        //Click the button
         test5button.click();
 
+        //Find success message and assert its display
         WebElement successMessage = driver.findElement(By.id("test5-alert"));
         Assert.assertTrue(successMessage.isDisplayed(), "Success message is not displayed after clicking the button.");
+        //Assert the button state after clicking
         Assert.assertFalse(test5button.isEnabled(), "The button is not disabled after clicking.");
     }
 
     @Test(priority = 6)
     public void testGridCellValueRetrieval() {
-       
+        //Retrieve value fo cell at coordinates (2, 2)
         String cellValue = getCellValue(2, 2);
+        //Assert the vlaue of the cell is "Ventosanzap"
         Assert.assertEquals(cellValue, "Ventosanzap", "Value of the cell at coordinates (2, 2) is not 'Ventosanzap'.");
     }
 
+    //Method to retrieve cell value from grid
     private String getCellValue(int row, int column) {
         String xpath = String.format("//table/tbody/tr[%d]/td[%d]", row + 1, column + 1);
         WebElement cell = driver.findElement(By.xpath(xpath));
@@ -117,6 +138,7 @@ public class Assignment {
 
     @AfterClass
     public void tearDown() {
+        //Quit webdriver instance
         if (driver != null) {
             driver.quit();
         }
